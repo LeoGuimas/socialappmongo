2a0b09a9e703d37d5edfbce740c497776e2b97f9
LeoGuimas <gleo.dev0@gmail.com>
2024-11-18 20:55:50 -0300

    Initialize Spring Boot project with Maven setup and basic configurations

---
+++ src/main/java/com/leoguimas/socialappmongo/SocialappmongoApplication.java
@@ -0,0 +1,13 @@
+package com.leoguimas.socialappmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialappmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialappmongoApplication.class, args);
+	}
+
+}
